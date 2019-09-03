1ed43de157061815b9502487dbc587778ed518bd
Etienne Landuré <elandure@gmail.com>
2019-09-03 16:18:41 +0200

    Call PredictFace with verbose set to `1`, train and save the current model

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 try:
     from model import PredictFace
 except ImportError as err:
-    exit(err)
+    exit("{}: {}".format(__file__, err))
 
 
 def main():
     """
     Main function.
     """
-    model = PredictFace()
+    model = PredictFace(verbose=1)
     # Create the model
     model.create_model()
+    # Learn
+    model.learn()
+    # Save this model
+    model.save()
 
 
 if __name__ == '__main__':
